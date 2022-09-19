0b340037189e73ee08a8841446eaeff687d348d0
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-09-19 12:03:02 -0500

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,10 +1,10 @@
 <?php require_once("header.php"); ?>
-<h1> Hello World </h1>
+<h1> Hello Everyone What A Beuatiful Day in 2022 </h1>
 <form action="handlepost1.php" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
-Favorite color: <input type="text" name="color"><br>
-Favorite animal: <input type="text" name="animal"><br>
+Favorite Sport: <input type="text" name="Sport"><br>
+Favorite Movie: <input type="text" name="Movie"><br>
 <input type="submit">
 </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
