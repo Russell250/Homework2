873e4a0627383ca9db722516b888cbe077067e61
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-09-19 12:04:34 -0500

    Update handlepost1.php

--- handlepost1.php
+++ handlepost1.php
@@ -10,8 +10,8 @@
   <body>
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?><br>
-    You really like the color, <? echo $_GET["color"]; ?><br>
-    Your favorite animal is <? echo $_GET["animal"]; ?>
+    You like the sport, <? echo $_GET["Sport"]; ?><br>
+    Your favorite movie is <? echo $_GET["Movie"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
