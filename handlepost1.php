072ecd1826da5ce57dd115324b24896f75cc866c
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-12-12 16:44:06 -0600

    Update handlepost1.php

--- handlepost1.php
+++ handlepost1.php
@@ -1,10 +1,11 @@
-
     <?php require_once("header.php"); ?>
+    <?php require_once("badgeGET.php"); ?>
+  </head>
   <body>
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?><br>
-    You like the sport, <? echo $_GET["Sport"]; ?><br>
-    Your favorite movie is <? echo $_GET["Movie"]; ?>
+    You really like the color, <? echo $_GET["color"]; ?><br>
+    Your favorite animal is <? echo $_GET["animal"]; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
