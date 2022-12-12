9bf4461bfa3f78fdf915082e9f650e5289e2abde
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-12-12 16:43:30 -0600

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -4,12 +4,19 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-      </head>
-
-<?php require_once("header.php"); ?>
+    <?php require_once("header.php"); ?>
+    <?php require_once("badgePOST.php"); ?>
+  </head>
   <body>
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
+<p>    <?php
+function writeMsg() {
+  echo "You are awesome!";
+}
+
+writeMsg();
+  ?> </p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
